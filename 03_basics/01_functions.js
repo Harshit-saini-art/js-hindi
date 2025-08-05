69d2bda79dddf9073385afab9e8d1d7aec20ce57
Harshit-saini-art <harshit.saini102005@gmail.com>
2025-08-05 08:06:45 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+
+function sayMyNamne(){
+    console.log("H");
+console.log("A");
+console.log("R");
+console.log("S");
+console.log("H");
+console.log("I");
+console.log("T");
+}
+
+// sayMyNamne()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    
+    let result = num
+}
+
+const result = addTwoNumbers(3, 5)
+
+console.log("Result:", result);
